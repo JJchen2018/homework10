acc119c19924d341e92441cbe2c1ef851a8d8e9a
JJchen2018 <1054051924@qq.com>
2018-06-10 15:43:44 +0800

    Initial commit

---
+++ 习题5-4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    char x;
+    int a,b,c,d;
+    a=0;
+    b=0;
+    c=0;
+    d=0;
+    printf("请输入一行字符 ");
+    while((x=getchar())!='\n')
+    {
+        if((x>='A'&&x<='Z')||(x>='a'&&x<='z'))
+        {
+        	a=a+1;
+        }
+        else if (x>='0'&&x<='9')
+        {
+        	b=b+1;
+        }
+        else if(x==32)
+        {
+        	c=c+1;
+        }
+        else
+        d=d+1;
+    }
+    printf("字母个数为%d\n",a);
+    printf("数字个数为%d\n",b);
+    printf("空格个数为%d\n",c);
+    printf("其他字符个数为%d\n",d);
+	return 0;
+}
